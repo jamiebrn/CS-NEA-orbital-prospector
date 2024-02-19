91e86dc6945e6374534f2c4a191f165db0248b62
JamzDev <82174996+Jamz25@users.noreply.github.com>
2024-02-19 22:02:30 +0000

    attempt at macro enum <-> string

--- include/Objects/ItemPickupType.hpp
+++ include/Objects/ItemPickupType.hpp
@@ -6,30 +6,48 @@
 
 #include "Manager/TextureManager.hpp"
 
+#define DECLARE_ITEM_PICKUP_TYPE_ENUM \
+    X(Rock) \
+    X(CopperChunk) \
+    X(IronChunk) \
+    X(CopperBar) \
+    X(IronBar) \
+    X(CopronBar) \
+    X(NONE)
+
 enum ItemPickupType
 {
-    Rock,
-    CopperChunk,
-    IronChunk,
-
-    CopperBar,
-    IronBar,
-    CopronBar,
-
-    NONE
+#define X(item) item,
+    DECLARE_ITEM_PICKUP_TYPE_ENUM
+#undef X
 };
 
 NLOHMANN_JSON_SERIALIZE_ENUM(ItemPickupType, 
 {
-    {Rock, "rock"},
-    {CopperChunk, "copper"},
-    {IronChunk, "iron"},
-    {CopperBar, "copperbar"},
-    {IronBar, "ironbar"},
-    {CopronBar, "copronbar"}
+#define X(item) {ItemPickupType::item, #item},
+    DECLARE_ITEM_PICKUP_TYPE_ENUM
+#undef X
 })
 
-const std::map<ItemPickupType, std::string> itemPickupToStringMap =
+std::string itemPickupTypeToString(ItemPickupType type)
+{
+    switch (type)
+    {
+    #define X(item) case ItemPickupType::item: return #item;
+        DECLARE_ITEM_PICKUP_TYPE_ENUM
+    #undef X
+    }
+}
+
+ItemPickupType stringToItemPickupType(std::string string)
+{
+    #define X(item) if (string == #item) return ItemPickupType::item;
+        DECLARE_ITEM_PICKUP_TYPE_ENUM
+    #undef X
+}
+
+/*
+const std::map<ItemPickupType, const char*> itemPickupToStringMap =
 {
     {ItemPickupType::Rock, "rock"},
     {ItemPickupType::CopperChunk, "copper"},
@@ -39,8 +57,6 @@ const std::map<ItemPickupType, std::string> itemPickupToStringMap =
     {ItemPickupType::CopronBar, "copronbar"}
 };
 
-
-
 const std::map<ItemPickupType, TextureType> itemTextureMap = {
     {ItemPickupType::Rock, TextureType::PickupRock},
     {ItemPickupType::CopperChunk, TextureType::PickupCopperChunk},
@@ -49,5 +65,6 @@ const std::map<ItemPickupType, TextureType> itemTextureMap = {
     {ItemPickupType::IronBar, TextureType::IronBar},
     {ItemPickupType::CopronBar, TextureType::CopronBar}
 };
+*/
 
 #endif
